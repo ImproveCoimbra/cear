c2eed472fc5f9fe914ec8b422ab76cdaf1078067
Sérgio Santos <me@sergiosantos.info>
2020-04-07 14:41:27 +0100

    Add open graph image

--- components/Head.js
+++ components/Head.js
@@ -13,12 +13,11 @@ export default ({ children }) => {
       <meta property="og:title" content={title} />
       <meta property="og:site_name" content={title} />
       <meta property="og:description" content={description} />
-      {/* <meta property="og:image" content="https://dinecph.dk/og-image.png" /> */}
+      <meta property="og:image" content="https://jantarada.pt/og-image.png" />
       <meta name="twitter:card" content="summary_large_image" />
-      {/* <meta name="twitter:site" content="" /> */}
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      {/* <meta name="twitter:image" content="https://dinecph.dk/og-image.png" /> */}
+      <meta property="twitter:image" content="https://jantarada.pt/og-image.png" />
       {children}
     </Head>
   )
